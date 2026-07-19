60e47beddd2a5be37a21c9b352f364bd0a98a128
daotrunghieu2k5 <daotrunghieup@gmail.com>
2026-07-19 10:03:16 +0700

    Initial commit: Setup PostgreSQL, Flyway and BaseEntity

---
+++ src/main/java/com/dthxhieu/ticket_booking_system_be/entity/JpaConfig.java
@@ -0,0 +1,9 @@
+package com.dthxhieu.ticket_booking_system_be.entity;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+
+@Configuration
+@EnableJpaAuditing
+public class JpaConfig {
+}
---
+++ src/test/java/com/dthxhieu/ticket_booking_system_be/TicketBookingSystemBeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dthxhieu.ticket_booking_system_be;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TicketBookingSystemBeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
